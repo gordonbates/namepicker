29b79824da59625bf20c21f71f226ba97fae5373
gordon_bates <gordonbatesofficial@gmail.com>
2022-10-31 16:30:56 +0000

    namepicker

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+names = [
+   "E&A", "R&J", "W&H"
+]
+
+godparents = random.randint(0, len(names) - 1)
+print(f"Godparents chosen: {names[godparents]}")
